9bf9dbf0ff9d2836538a46d5ef5a4016d8f39782
AAweidai <1253464613@qq.com>
2021-11-02 12:04:50 +0800

    Fix dependency

--- chain/src/transaction.rs
+++ chain/src/transaction.rs
@@ -149,6 +149,13 @@ pub struct TransactionOutput {
     pub script_pubkey: Bytes,
 }
 
+#[derive(Ord, PartialOrd, Eq, PartialEq, Clone, Debug)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Serializable, Deserializable)]
+pub struct TransactionOutputArray {
+    pub outputs: Vec<TransactionOutput>,
+}
+
 impl Default for TransactionOutput {
     fn default() -> Self {
         TransactionOutput {
@@ -392,36 +399,36 @@ mod tests {
     fn test_transaction_reader_with_witness() {
         let actual: Transaction = "01000000000102fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f00000000494830450221008b9d1dc26ba6a9cb62127b02742fa9d754cd3bebf337f7a55d114c8e5cdd30be022040529b194ba3f9281a99f2b1c0a19c0489bc22ede944ccf4ecbab4cc618ef3ed01eeffffffef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a0100000000ffffffff02202cb206000000001976a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac9093510d000000001976a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac000247304402203609e17b84f6a7d30c80bfa610b5b4542f32a8a0d5447a12fb1366d7f01cc44a0220573a954c4518331561406f90300e8f3358f51928d43c212a8caed02de67eebee0121025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee635711000000".parse().unwrap();
         let expected = Transaction {
-			version: 1,
-			inputs: vec![TransactionInput {
-				previous_output: OutPoint {
-					txid: h256("fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f"),
-					index: 0,
-				},
-				script_sig: "4830450221008b9d1dc26ba6a9cb62127b02742fa9d754cd3bebf337f7a55d114c8e5cdd30be022040529b194ba3f9281a99f2b1c0a19c0489bc22ede944ccf4ecbab4cc618ef3ed01".parse().unwrap(),
-				sequence: 0xffffffee,
-				script_witness: vec![],
-			}, TransactionInput {
-				previous_output: OutPoint {
+            version: 1,
+            inputs: vec![TransactionInput {
+                previous_output: OutPoint {
+                    txid: h256("fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f"),
+                    index: 0,
+                },
+                script_sig: "4830450221008b9d1dc26ba6a9cb62127b02742fa9d754cd3bebf337f7a55d114c8e5cdd30be022040529b194ba3f9281a99f2b1c0a19c0489bc22ede944ccf4ecbab4cc618ef3ed01".parse().unwrap(),
+                sequence: 0xffffffee,
+                script_witness: vec![],
+            }, TransactionInput {
+                previous_output: OutPoint {
                     txid: h256("ef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a"),
-					index: 1,
-				},
-				script_sig: "".parse().unwrap(),
-				sequence: 0xffffffff,
-				script_witness: vec![
-					"304402203609e17b84f6a7d30c80bfa610b5b4542f32a8a0d5447a12fb1366d7f01cc44a0220573a954c4518331561406f90300e8f3358f51928d43c212a8caed02de67eebee01".parse().unwrap(),
-					"025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee6357".parse().unwrap(),
-				],
-			}],
-			outputs: vec![TransactionOutput {
-				value: 0x0000000006b22c20,
-				script_pubkey: "76a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac".parse().unwrap(),
-			}, TransactionOutput {
-				value: 0x000000000d519390,
-				script_pubkey: "76a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac".parse().unwrap(),
-			}],
-			lock_time: 0x00000011,
-		};
+                    index: 1,
+                },
+                script_sig: "".parse().unwrap(),
+                sequence: 0xffffffff,
+                script_witness: vec![
+                    "304402203609e17b84f6a7d30c80bfa610b5b4542f32a8a0d5447a12fb1366d7f01cc44a0220573a954c4518331561406f90300e8f3358f51928d43c212a8caed02de67eebee01".parse().unwrap(),
+                    "025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee6357".parse().unwrap(),
+                ],
+            }],
+            outputs: vec![TransactionOutput {
+                value: 0x0000000006b22c20,
+                script_pubkey: "76a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac".parse().unwrap(),
+            }, TransactionOutput {
+                value: 0x000000000d519390,
+                script_pubkey: "76a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac".parse().unwrap(),
+            }],
+            lock_time: 0x00000011,
+        };
         assert_eq!(actual, expected);
     }
 
