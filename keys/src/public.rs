6fe403854e0c6ed9acdd59c2de723094d1307ab9
AAweidai <1253464613@qq.com>
2021-12-22 13:03:39 +0800

    Add try from between public

--- keys/src/public.rs
+++ keys/src/public.rs
@@ -83,6 +83,13 @@ impl TryFrom<Public> for musig2::PublicKey {
     }
 }
 
+impl TryFrom<musig2::PublicKey> for Public {
+    type Error = Error;
+    fn try_from(p: musig2::PublicKey) -> Result<Self, Self::Error> {
+        Public::from_slice(&p.serialize())
+    }
+}
+
 impl Public {
     pub fn from_slice(data: &[u8]) -> Result<Self, Error> {
         match data.len() {
